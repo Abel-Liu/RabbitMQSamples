c7ef91ca5a654407033bed5ed29b753f54a71e11
Abel Liu <abel.liu.china@outlook.com>
2016-12-16 16:58:56 +0800

    subscribe

---
+++ Subscribe/Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queueName, "logs", "");
+
+                Console.WriteLine("Waiting for receive...");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, e) =>
+                {
+                    var body = e.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received: " + message);
+                };
+
+                channel.BasicConsume(queue: queueName,
+                                     noAck: true,
+                                     consumer: consumer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Subscribe/Producer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                var message = "hello world";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs", "", null, body);
+
+                Console.WriteLine("Sent " + message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
