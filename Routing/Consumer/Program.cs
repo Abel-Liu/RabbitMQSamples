85760e1bf3d6c520854822de9f83a2a3f573d2d1
Abel Liu <abel.liu.china@outlook.com>
2016-12-16 18:19:32 +0800

    routing

---
+++ Routing/Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("direct_logs", "direct");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                //不同实例订阅若干不同routing，可实现消息过滤
+                channel.QueueBind(queueName, "direct_logs", "info");
+                channel.QueueBind(queueName, "direct_logs", "warning");
+                channel.QueueBind(queueName, "direct_logs", "error");
+
+                Console.WriteLine("Waiting for messages...");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, e) =>
+                {
+                    var body = e.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = e.RoutingKey;
+                    Console.WriteLine("Received {0}: {1}", routingKey, message);
+                };
+                channel.BasicConsume(queueName, true, consumer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Routing/Producer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("direct_logs", "direct");
+
+                string[] levels = new string[] { "info", "warning", "error" };
+                int i = 0;
+
+                while (i < 10)
+                {
+                    var level = levels[i % 3];
+
+                    var message = level + " at " + DateTime.Now.ToString("mm:ss.fff");
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("direct_logs", level, null, body);
+                    Console.WriteLine("Sent " + message);
+
+                    i++;
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
